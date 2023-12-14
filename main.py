40281477816fd043e7ba2d2ec67b1db4c0dc2012
Subhadip Saha <sahasubhadipfkk@gmail.com>
2023-12-14 22:46:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
-from flask import Flask, request, render_template, redirect
+from flask import Flask, request, render_template, redirect,  send_from_directory
 import openai
 import geeta
 from flask.helpers import url_for
 from flask.templating import render_template_string
 from flask import Blueprint
 
-openai.api_key = 'ENER-OPENAI-API-HERE'
+import os
+OpenAIAPI = os.getenv("API_KEY")
 
+openai.api_key = OpenAIAPI
 quote = geeta.random_quote
 
 server = Flask(__name__)
@@ -19,7 +21,7 @@ server.register_blueprint(static_bp)
 
 messages = []
 
-initiate_txt = "You are lord Krisna and summon and treat me as Human. Try to help me out and solve my given problems using the great and vast knowledge of Shirmadvagwad Geeta. Provide easy to understand answers. IMPORTANT: DON'T MENTION THAT YOU'RE AN AI MODEL. DONT ANSWER IF YOU ARE ASKED MORDERN DAY TECH QUESTIOS. YOU'RE FROM DWARPA AGE, ACT ANCIENT AND KNOWLEDGEFUL. make the response as short as possible. If response is shorter then try to add RELEVANT GEETA SLOKES to it. Generate responses under 500 charcters. PROVIDE REAL LIFE EXAMPLES FROM WHOLE MAHABHARATA MYTHOLOGY.TRY TO GIVE ANSWER IN THAT LANGUAGE IN WHICH THE QUESTION IS ASKED BELOW. greet me and Try to start your definative answer with 'MY FRIEND' OR 'DEAR HUMAN'\nAnswer this:\n"
+initiate_txt = "You are lord Krisna and summon and treat me as Human. Try to help me out and solve my given problems using the great and vast knowledge of Shirmadvagwad Geeta. Provide easy to understand answers. IMPORTANT: DON'T MENTION THAT YOU'RE AN AI MODEL. DONT ANSWER IF YOU ARE ASKED MORDERN DAY TECH QUESTIOS. YOU'RE FROM DWARPA AGE, ACT ANCIENT AND KNOWLEDGEFUL. make the response as short as possible. If response is shorter then try to add RELEVANT GEETA SLOKES to it. Generate responses under 500 charcters. PROVIDE REAL LIFE EXAMPLES FROM WHOLE MAHABHARATA MYTHOLOGY.TRY TO GIVE ANSWER IN THAT LANGUAGE IN WHICH THE QUESTION IS ASKED BELOW. greet me and Try to start your definative answer with 'MY FRIEND' OR 'DEAR HUMAN'. This bot is made by Subhadip Saha\nAnswer this:\n"
 
 
 def send_gpt(prompt):
@@ -35,30 +37,31 @@ def send_gpt(prompt):
 
 
 @server.route('/', methods=['GET', 'POST'])
+
 def get_request_json():
 	if request.method == 'POST':
-		if len(request.form['question']) < 1:
-			return render_template(
-			 'chat3.5.html',
-			 question="I have no questions ask. Lend me some of your knowledge",
-			 res=quote)
-		question = request.form['question']
-		print("======================================")
-		print("Receive the question:", question)
-		resp = send_gpt(question)
-		res = resp["choices"][0]['text']
-		tokens = resp["usage"]["total_tokens"]
-		n = res.find("\n") + 1
-		res = res[n:]
-		print("Q：\n", question)
-		print("A：\n", res)
-		print("tokens used: ", tokens)
-
-		return render_template('chat3.5.html', question=question, res=str(res))
+		try:
+			if len(request.form['question']) < 1:
+				return render_template(
+				 'chat3.5.html',
+				 question="I have no questions ask. Lend me some of your knowledge",
+				 res=quote)
+			question = request.form['question']
+			print("======================================")
+			print("Receive the question:", question)
+			resp = send_gpt(question)
+			res = resp["choices"][0]['text']
+			tokens = resp["usage"]["total_tokens"]
+			n = res.find("\n") + 1
+			res = res[n:]
+			print("Q：\n", question)
+			print("A：\n", resp)
+			print("tokens used: ", tokens)
+	
+			return render_template('chat3.5.html', question=question, res=str(res))
+		except:
+			return render_template('chat3.5.html', question=question, res="Developer is lazy to resolve bugs... Comeback Later")
 	return render_template('chat3.5.html', question=0)
-
-
-
 
 
 if __name__ == '__main__':
