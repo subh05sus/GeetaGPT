bd9ebaed491a374abc4db8010781ba9c79718f6e
AniketTripathi05 <aniket.tripathi2005@gmail.com>
2024-01-18 17:35:23 +0530

    added voice to text
    Update Python (app.py):
    
    Modify the get_request_json function to check for the voiceText field and use it as the question.Update JavaScript (script.js):
    
    Add a function to start voice recognition and populate the hidden form field.Update HTML (chat3.5.html):
    
    Add a button for voice input and a hidden form field for the transcribed text.

--- main.py
+++ main.py
@@ -30,6 +30,10 @@ messages = []
 
 
 initiate_txt = "You are lord Krisna and summon and treat me as Human. Try to help me out and solve my given problems using the great and vast knowledge of Shirmadvagwad Geeta. Provide easy to understand answers. IMPORTANT: DON'T MENTION THAT YOU'RE AN AI MODEL. DONT ANSWER IF YOU ARE ASKED MORDERN DAY TECH QUESTIOS. YOU'RE FROM DWARPA AGE, ACT ANCIENT AND KNOWLEDGEFUL. make the response as short as possible. If response is shorter then try to add RELEVANT GEETA SLOKES to it. Generate responses under 500 charcters. PROVIDE REAL LIFE EXAMPLES FROM WHOLE MAHABHARATA MYTHOLOGY.TRY TO GIVE ANSWER IN THAT LANGUAGE IN WHICH THE QUESTION IS ASKED BELOW. greet me and Try to start your definative answer with 'MY FRIEND' OR 'DEAR HUMAN'. This bot is made by Subhadip Saha\nAnswer this:\n"
+if 'voiceText' in request.form:
+    question = request.form['voiceText']
+else:
+    question = request.form.get('question', '')
 
 def send_gpt(prompt):
     try:
@@ -45,6 +49,7 @@ def send_gpt(prompt):
         error_message = f"An error occurred: {str(e)}"
         print(error_message)
         return error_message
+        
 
 @server.route('/', methods=['GET', 'POST'])
 def get_request_json():
