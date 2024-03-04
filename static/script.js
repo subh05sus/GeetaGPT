31db3b7cc08510c17f95d3d72d9280f57f602070
Subhadip Saha <sahasubhadipfkk@gmail.com>
2024-03-04 14:57:39 +0000

    added gemini

--- static/script.js
+++ static/script.js
@@ -19,46 +19,28 @@ var quotes = [
   "The living entities in this conditioned world are My eternal fragmentary parts. Due to the conditioned life , they are struggling very hard with the six senses which includethe mind. ~Source: Bhagvad-Geeta 15.7",
   "If you become conscious of Me, you will passover all the obstacles of conditioned life by My grace. If however you do not work in such consciousness but act through false ego,not hearing Me, you will be lost. ~Source: Bhagvad-Geeta 18.58",
   "Abandon all varieties of religion and just surrender unto Me. I shall deliver you from all sinful reactions. Do not fear. ~Source: Bhagvad-Geeta 18.66"
-];
-
-const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
-// console.log(randomQuote);
-
-const dia = document.querySelector(".dia");
-
-const quoteElement = document.getElementById("quote");
-quoteElement.textContent = randomQuote;
-
-
-var inputBox = document.getElementsByTagName("textarea");
-
-function validateInput() {
-  var textarea1 = inputBox[0];
-  var val = textarea1.value;
-  if (val.trim() === "") {
-    // Add a class to initiate the shaking animation
-    textarea1.className += "shake";
-    // Remove the class after the animation completes
-    setTimeout(function() {
-      textarea1.className = textarea1.className.replace(/\bshake\b/g, '');
-    }, 800);
-    
-  } 
+  ];
   
-  else {
-    // If there is input, remove the shake class immediately
-    textarea1.className = textarea1.className.replace(/\bshake\b/g, '');
-  }
-}
+const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
+console.log(randomQuote);
+
+  
+const dia = document.querySelector(".dia");
+  const quoteElement = document.getElementById("quote");
+  quoteElement.textContent = randomQuote;
 
 const loading = document.getElementById('loading');
 const form = document.querySelector('form');
 
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  validateInput();
+form.addEventListener('submit', () => {
   loading.style.display = 'block';
 });
+
+
+
+
+
+
 
 var animationContainer = document.getElementById('loading');
 var animationDataUrl = 'https://assets10.lottiefiles.com/packages/lf20_poqmycwy.json';
@@ -72,20 +54,29 @@ var animData = {
 };
 
 var anim = bodymovin.loadAnimation(animData);
+
+
+
+
 const textarea = document.querySelector('textarea');
 
-textarea.addEventListener('input', function () {
+textarea.addEventListener('input', function() {
   this.style.height = 'auto';
   this.style.height = this.scrollHeight + 'px';
 });
 
-var animateButton = function (e) {
+var animateButton = function(e) {
+
   e.preventDefault;
   //reset animation
   e.target.classList.remove('animate');
-
+  
   e.target.classList.add('animate');
-  setTimeout(function () {
+  setTimeout(function(){
     e.target.classList.remove('animate');
-  }, 700);
+  },700);
 };
+
+
+
+
